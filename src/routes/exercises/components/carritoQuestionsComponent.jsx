973354191ee0ed7aef2e86e15a30a4bd60ff3a81
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-01-13 13:11:22 -0500

    ejercicio 64 finish

--- src/routes/exercises/components/carritoQuestionsComponent.jsx
+++ src/routes/exercises/components/carritoQuestionsComponent.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useLayoutEffect } from "react";
 import carrito from "/images/exercises/64/carrito.svg"
 import styles from "../../../styles/carritoQuestions.module.css"
-import pista from "/images/exercises/64/pista.svg"
+
 
 const Pista = ({corrects})=>{
     return(
@@ -12,38 +12,110 @@ const Pista = ({corrects})=>{
 
 }
 
-const Card = ({id, question, value})=>{
-    
+const Card = ({id, question, value, isFinish,setAnswers})=>{
+    const [selection, setSelection] = useState(null)
+    const [isSelection, setIsSelection] = useState(false)
+
+    const handleClick = (selected)=>{
+        if(!isFinish){
+            setIsSelection(true)
+            setSelection(selected)
+            setAnswers(
+                prev => (
+                    prev.map( ans =>{
+                        if(ans.id == id){
+                            return {id:ans.id, answer:selected, isSelected:true}
+                        }
+                        return ans
+                    })
+            )
+        )
+        }
+
+    }
+        
     return (
         <div className={styles.card}>
             <div className={styles.cardSentence}>{question}</div>
             <div className={styles.cardButtonsContainer}>
-                <div className={styles.cardButton}>F</div>
-                <div className={styles.cardButton}>V</div>
+                <div className={isSelection&&!selection?styles.buttonPress:styles.cardButton} style={isFinish&&!selection?(selection==value?{backgroundColor:"#69E485"}:{backgroundColor:"#FF7171"}):null} onClick={()=>{handleClick(false)}}>F</div>
+                <div className={isSelection&&selection?styles.buttonPress:styles.cardButton} style={isFinish&&selection?(selection==value?{backgroundColor:"#69E485"}:{backgroundColor:"#FF7171"}):null} onClick={()=>{handleClick(true)}}>V</div>
             </div>
         </div>
     )
 }
 
-const GridCards = ()=>{
+const GridCards = ({questions,setAnswers,isFinish})=>{
+    return(
+        <div className={styles.gridCard}>
+            {
+                questions.map(
+                    question =>(<Card key={question.id} id={question.id} question={question.question} value={question.value} setAnswers={setAnswers} isFinish={isFinish}/>)
+                )
+            }
+        </div>
+    )
 
 }
 
-const CarritoQuestionsComponent = ({data})=>{
+const CarritoQuestionsComponent = ({data,setPhase,setScore})=>{
     const [questions, setQuestions] = useState(data.questions)
+    const [answers, setAnswers] = useState(questions.map(
+        q => ({id: q.id, answer:null, isSelected:false})
+    ))
+    const [answered, setAnswered] = useState(0)
+    const [isFinish, setIsFinish] = useState(false)
+    const [corrects, setCorrects] = useState(0)
 
-    const sortQuestions = ()=>{
-        const shuffleQuestions = questions.sort( () => Math.random() - 0.5).map(
-            question=>({
-                ...question, isSelected: false
-            })
-        )
-        setQuestions(shuffleQuestions)
+    function selectRandomElements(arr, num) {
+        let selectedElements = [];
+        for (let i = 0; i < num; i++) {
+            let randomIndex = Math.floor(Math.random() * arr.length);
+            selectedElements.push(arr[randomIndex]);
+            arr.splice(randomIndex, 1);
+        }
+        return selectedElements;
     }
 
+    const sortQuestions = ()=>{
+        const shuffleQuestions = [...questions]
+        shuffleQuestions.sort( () => Math.random() - 0.5)
+        setQuestions(shuffleQuestions)
+    }
+    
+    useEffect(()=>{
+        sortQuestions()
+    },[])
+    useEffect(()=>{
+        console.log(answers)
+    },[answers])
+
+    useEffect(()=>{
+        setAnswered(answers.filter((ans) => ans.isSelected === true).length)
+    },[answers])
+
+    const handleResponder = ()=>{
+        answers.map(ans => {
+            const correct = questions.filter(q => q.id == ans.id)[0].value == ans.answer
+                       
+            if (correct){
+                setCorrects(prev => prev+1)
+            }
+
+        })
+        setIsFinish(true)
+    }
+    useEffect(()=>{console.log(corrects)},[corrects])
+    useEffect(()=>{
+        setScore(corrects)
+    },[isFinish])
+
     return(
-        <div>
-            <Pista corrects={4} />
+        <div className={styles.gameContainer}>
+            <Pista corrects={corrects} />
+            <GridCards questions={questions} setAnswers={setAnswers} isFinish={isFinish}/>
+            {answered == questions.length && !isFinish?<button onClick={handleResponder}>RESPONDER</button>:<></>}
+            {isFinish?<button onClick={()=>{setPhase("end")}}>SIGUIENTE</button>:<></>}
         </div>
     )
 
