900d1276156593fecaabd0d6191b7974f7ea9fae
Daniel Dorado <danieldorado27@gmail.com>
2023-01-31 08:43:30 -0500

    [CREATE] GradeInfoCard done, pendding some functionalities like pencil button

--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -1,10 +1,30 @@
 import HorizontalBar from '../../components/horizontalBar'
+import GradeInfoCard from '../../components/gradeInfoCard'
 // import data from './data.json'
 
 export default function ExTest () {
+  const listaGrados = [
+    {
+      grade: '6 A',
+      teacher: 'Ramirez'
+    },
+    {
+      grade: '6 B',
+      teacher: 'Ramirez'
+    },
+    {
+      grade: '6 C',
+      teacher: 'Ramirez'
+    },
+    {
+      grade: '6 D',
+      teacher: 'Ramirez'
+    }
+  ]
   return (
     <>
       <HorizontalBar label='Hola' />
+      <GradeInfoCard title='Grado sexto' lista={listaGrados} />
     </>
   )
 }
