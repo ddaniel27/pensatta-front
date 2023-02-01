0208756f894f1c321f86fdc80deaeaabf7d3d283
Daniel Dorado <danieldorado27@gmail.com>
2023-02-01 10:57:04 -0500

    [CREATE] dashboardHorizontalBar created, responsive still needed

--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -1,7 +1,7 @@
 // import HorizontalBar from '../../components/horizontalBar'
 // import GradeInfoCard from '../../components/gradeInfoCard'
 // import TableManageCourse from '../../components/tableManageCourse'
-import DashboardStudentIndividual from '../../components/teacherProfile/dashboardStudentIndividual'
+import DashboardStudentIndividual from '../../components/teacherProfile/dashboardHorizontalBar'
 // import data from './data.json'
 
 export default function ExTest () {
