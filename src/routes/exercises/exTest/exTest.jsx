a4a608e7a594a6f3045d30fc879a8692b57a6805
Daniel Dorado <danieldorado27@gmail.com>
2023-02-01 11:57:48 -0500

    [CREATE] dashboardCardsHorizontalRows created to dashboard new view

--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -1,7 +1,7 @@
 // import HorizontalBar from '../../components/horizontalBar'
 // import GradeInfoCard from '../../components/gradeInfoCard'
 // import TableManageCourse from '../../components/tableManageCourse'
-import DashboardStudentIndividual from '../../components/teacherProfile/dashboardHorizontalBar'
+import DashboardStudentIndividual from '../../components/teacherProfile/dashboardCardsHorizontalRows'
 // import data from './data.json'
 
 export default function ExTest () {
