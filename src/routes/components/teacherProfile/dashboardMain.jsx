768860b818848fcc1a8ef4ff5463d739bcaab0eb
Daniel Dorado <danieldorado27@gmail.com>
2023-02-01 17:16:26 -0500

    [UPDATE] DashboardMain done mockup

--- src/routes/components/teacherProfile/dashboardMain.jsx
+++ src/routes/components/teacherProfile/dashboardMain.jsx
@@ -1,5 +1,8 @@
 import HeaderMain from '../headerMainTeacherCoordinator'
 import FooterTeacherCoordinatorView from '../footerTeacherCoordinatorView'
+import CardHorizontalRow from '../CardHorizontalRow'
+import PieChart from './pieChart'
+import pencil from '../../../../public/images/Atomo_Icono_Editar.svg'
 import '../../../styles/dashboardMain.css'
 
 export default function DashboardMain () {
@@ -7,8 +10,65 @@ export default function DashboardMain () {
     <div className='DashboardMain'>
       <HeaderMain />
       <div className='DashboardMain__content'>
+        <CardHorizontalRow {...defaultData} />
+        <div className='DashboardMain__content__group'>
+          <div className='DashboardMain__content__group__graphs'>
+            <PieChart pieValues={{ 0: 12, 1: 15, 2: 20 }} />
+            <PieChart pieValues={{ 0: 12, 1: 15, 2: 20 }} />
+          </div>
+          <div className='DashboardMain__content__group__buttons'>
+            <div className='DashboardMain__content__group__buttons__pdf'>DESCARGAR INFORME EN PDF</div>
+            <div className='DashboardMain__content__group__buttons__teachers'>
+              <span>Listado docentes</span>
+              <img src={pencil} alt='pencil' />
+            </div>
+          </div>
+        </div>
       </div>
       <FooterTeacherCoordinatorView />
     </div>
   )
+}
+
+const defaultData = {
+  title: 'Grado',
+  average: 5,
+  rows: [
+    {
+      label: '6',
+      valueGreen: 0.3,
+      valueYellow: 0.2,
+      valueRed: 0.5
+    },
+    {
+      label: '7',
+      valueGreen: 0.5,
+      valueYellow: 0.3,
+      valueRed: 0.2
+    },
+    {
+      label: '8',
+      valueGreen: 0.2,
+      valueYellow: 0.5,
+      valueRed: 0.3
+    },
+    {
+      label: '9',
+      valueGreen: 0.3,
+      valueYellow: 0.2,
+      valueRed: 0.5
+    },
+    {
+      label: '10',
+      valueGreen: 0.34,
+      valueYellow: 0.33,
+      valueRed: 0.33
+    },
+    {
+      label: '11',
+      valueGreen: 0.2,
+      valueYellow: 0.2,
+      valueRed: 0.6
+    }
+  ]
 }
--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -2,6 +2,7 @@
 // import GradeInfoCard from '../../components/gradeInfoCard'
 // import TableManageCourse from '../../components/tableManageCourse'
 import DashboardStudentIndividual from '../../components/teacherProfile/dashboardMain'
+// import DashboardStudentIndividual from '../../components/teacherProfile/dashboardCardsHorizontalRows'
 // import data from './data.json'
 
 export default function ExTest () {
