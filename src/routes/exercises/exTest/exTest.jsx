c9d5c99f71eda8d7eb994e87a4b33e620c0f674c
Daniel Dorado <danieldorado27@gmail.com>
2023-02-05 12:22:50 -0500

    [UPDATE] dashboardStudentIndividual responsive for mobile only added

--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -1,13 +1,13 @@
 // import HorizontalBar from '../../components/horizontalBar'
 // import GradeInfoCard from '../../components/gradeInfoCard'
 
-import DashboardStudentIndividual from '../../components/teacherProfile/dashboardMain'
+// import DashboardStudentIndividual from '../../components/teacherProfile/dashboardMain'
 // import DashboardStudentIndividual from '../../components/teacherProfile/dashboardListGrades'
 // import DashboardStudentIndividual from '../../components/tableManageCourse'
 // import DashboardStudentIndividual from '../../components/teacherProfile/dashboardCardsHorizontalRows'
 // import DashboardStudentIndividual from '../../components/teacherProfile/dashboardHorizontalBar'
 // import DashboardStudentIndividual from "../../components/teacherProfile/dashboardRows"
-// import DashboardStudentIndividual from "../../components/teacherProfile/dashboardStudentIndividual"
+import DashboardStudentIndividual from "../../components/teacherProfile/dashboardStudentIndividual"
 // import data from './data.json'
 
 export default function ExTest () {
