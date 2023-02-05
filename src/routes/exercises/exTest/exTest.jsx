5341acac317c3e421d8964febe40bbfbfe906a13
Daniel Dorado <danieldorado27@gmail.com>
2023-02-05 12:30:20 -0500

    [UPDATE] dashboardRows added responsive for mobile only

--- src/routes/exercises/exTest/exTest.jsx
+++ src/routes/exercises/exTest/exTest.jsx
@@ -6,8 +6,8 @@
 // import DashboardStudentIndividual from '../../components/tableManageCourse'
 // import DashboardStudentIndividual from '../../components/teacherProfile/dashboardCardsHorizontalRows'
 // import DashboardStudentIndividual from '../../components/teacherProfile/dashboardHorizontalBar'
-// import DashboardStudentIndividual from "../../components/teacherProfile/dashboardRows"
-import DashboardStudentIndividual from "../../components/teacherProfile/dashboardStudentIndividual"
+import DashboardStudentIndividual from '../../components/teacherProfile/dashboardRows'
+// import DashboardStudentIndividual from '../../components/teacherProfile/dashboardStudentIndividual'
 // import data from './data.json'
 
 export default function ExTest () {
