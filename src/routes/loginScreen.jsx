1d3625d31c3f8a4a458d4c4cf10ae42befb7c944
Daniel Dorado <danieldorado27@gmail.com>
2023-02-06 14:21:03 -0500

    [UPDATE] Last exercises

--- src/routes/loginScreen.jsx
+++ src/routes/loginScreen.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import LoginForm from './components/loginForm'
 import '../styles/loginScreen.css'
 
-export default function LoginScreen(){
-    return (
-        <div className='login-screen'>
-            <img src='./images/Group.svg' alt='logo-pensatta'/>
-            <LoginForm />
-        </div>
-    )
+export default function LoginScreen () {
+  return (
+    <div className='login-screen'>
+      <img src='./images/Group.svg' alt='logo-pensatta' />
+      <LoginForm />
+    </div>
+  )
 }
 
+/*
+30, 36, 42, 45, 49, 52, 56, 65, 66, 67, 70, 71, 72, 74, 76, 80, 83, 90
+91, 92, 93, 94, 96
+  */
