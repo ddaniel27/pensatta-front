67fdfc4a390fd64146f072d34caa92f196b6be65
Daniel Dorado <danieldorado27@gmail.com>
2023-02-06 22:36:58 -0500

    [CREATE] Ex67 done

--- src/routes/exercises/ex67/ex67.jsx
+++ src/routes/exercises/ex67/ex67.jsx
@@ -5,8 +5,7 @@ import data from './data.json'
 import '../../../styles/ex10.css'
 
 export default function Ex37 () {
-  // eslint-disable-next-line no-unused-vars
-  const [myData, setMyData] = React.useState({
+  const [myData] = React.useState({
     ...data,
     patterns: data.patterns.sort(() => 0.5 - Math.random()).slice(0, 1)
   })
@@ -41,7 +40,7 @@ export default function Ex37 () {
   React.useEffect(() => {
     async function test () {
       while (!rojoRef.current && !verdeRef.current && !amarilloRef.current && !rojoObjectiveRef.current && !verdeObjectiveRef.current && !amarilloObjectiveRef.current) {
-        await new Promise(r => setTimeout(r, 500))
+        await new Promise(resolve => setTimeout(resolve, 500))
         console.log('waiting...')
       }
       setRojo(rojoRef.current)
@@ -125,47 +124,47 @@ export default function Ex37 () {
   return (
     <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name} threshold={myData.threshold} exerciseId={myData.id}>
       {
-                (setScore, setPhase) => (
-                  <div className='maze-style'>
-                    <DndComponent data={myData} returnScore={setOptionsData} reset={reset} />
-                    <div className='info-box'>
-                      <div>
-                        <svg width='180' height='80' viewBox='0 0 180 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                          <g id='Semaforo 1'>
-                            <rect x='2' y='2' width='176' height='76' rx='38' fill='#333333' />
-                            <ellipse cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#EDCA71' />
-                            <ellipse cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#FF7171' />
-                            <ellipse cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#69E485' />
-                            <ellipse ref={verdeObjectiveRef} cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                            <ellipse ref={amarilloObjectiveRef} cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                            <ellipse ref={rojoObjectiveRef} cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                            <rect x='2' y='2' width='176' height='76' rx='38' stroke='#F2F2F2' strokeWidth='4' />
-                          </g>
-                        </svg>
-                        <p onClick={playPattern} />
-                      </div>
-                      <svg width='360' height='160' viewBox='0 0 180 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                        <g id='Sem&#195;&#161;foro 1'>
-                          <rect x='2' y='2' width='176' height='76' rx='38' fill='#333333' />
-                          <ellipse cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#EDCA71' />
-                          <ellipse cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#FF7171' />
-                          <ellipse cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#69E485' />
-                          <ellipse ref={verdeRef} cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                          <ellipse ref={amarilloRef} cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                          <ellipse ref={rojoRef} cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#2F2F2F' fillOpacity='0.5' />
-                          <rect x='2' y='2' width='176' height='76' rx='38' stroke='#F2F2F2' strokeWidth='4' />
-                        </g>
-                      </svg>
-                    </div>
+        (setScore, setPhase) => (
+          <div className='maze-style-single'>
+            <DndComponent data={myData} returnScore={setOptionsData} reset={reset} />
+            <div className='info-box'>
+              <div className='two-objects-grid-spaced'>
+                <svg width='180' height='80' viewBox='0 0 180 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                  <g id='Semaforo 1'>
+                    <rect x='2' y='2' width='176' height='76' rx='38' fill='#333333' />
+                    <ellipse cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#EDCA71' />
+                    <ellipse cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#FF7171' />
+                    <ellipse cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#69E485' />
+                    <ellipse ref={verdeObjectiveRef} cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                    <ellipse ref={amarilloObjectiveRef} cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                    <ellipse ref={rojoObjectiveRef} cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                    <rect x='2' y='2' width='176' height='76' rx='38' stroke='#F2F2F2' strokeWidth='4' />
+                  </g>
+                </svg>
+                <p onClick={playPattern} />
+              </div>
+              <svg width='360' height='160' viewBox='0 0 180 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <g id='Sem&#195;&#161;foro 1'>
+                  <rect x='2' y='2' width='176' height='76' rx='38' fill='#333333' />
+                  <ellipse cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#EDCA71' />
+                  <ellipse cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#FF7171' />
+                  <ellipse cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#69E485' />
+                  <ellipse ref={verdeRef} cx='36' cy='41' rx='15' ry='15' transform='rotate(-90 36 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                  <ellipse ref={amarilloRef} cx='90' cy='41' rx='15' ry='15' transform='rotate(-90 90 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                  <ellipse ref={rojoRef} cx='141' cy='41' rx='15' ry='15' transform='rotate(-90 141 41)' fill='#2F2F2F' fillOpacity='0.5' />
+                  <rect x='2' y='2' width='176' height='76' rx='38' stroke='#F2F2F2' strokeWidth='4' />
+                </g>
+              </svg>
+            </div>
 
-                    <div className='buttons-field'>
-                      {optionsData.length > 0 && <button onClick={handleReset} className='restart'>REINTENTAR</button>}
-                      {!start && <button onClick={() => { handleClick(setScore) }} disabled={!optionsData.length}>INICIAR</button>}
-                      {finished && <button onClick={() => { setPhase('end') }} disabled={!optionsData.length}>FINALIZAR</button>}
-                    </div>
-                  </div>
-                )
-            }
+            <div className='buttons-field'>
+              {optionsData.length > 0 && <button onClick={handleReset} className='restart'>REINTENTAR</button>}
+              {!start && <button onClick={() => { handleClick(setScore) }} disabled={!optionsData.length}>INICIAR</button>}
+              {finished && <button onClick={() => { setPhase('end') }} disabled={!optionsData.length}>FINALIZAR</button>}
+            </div>
+          </div>
+        )
+      }
     </ScoringComponent>
   )
 }
--- src/routes/exercises/routerActivity.jsx
+++ src/routes/exercises/routerActivity.jsx
@@ -56,6 +56,7 @@ import Ex62 from './ex62/ex62'
 import Ex63 from './ex63/ex63'
 import Ex64 from './ex64/ex64'
 import Ex66 from './ex66/ex66'
+import Ex67 from './ex67/ex67'
 import Ex68 from './ex68/ex68'
 import Ex69 from './ex69/ex69'
 import Ex73 from './ex73/ex73'
@@ -147,6 +148,7 @@ export default function RouterActivity ({ idExercise }) {
       {idExercise === 63 && <Ex63 />}
       {idExercise === 64 && <Ex64 />}
       {idExercise === 66 && <Ex66 />}
+      {idExercise === 67 && <Ex67 />}
       {idExercise === 68 && <Ex68 />}
       {idExercise === 69 && <Ex69 />}
       {idExercise === 73 && <Ex73 />}
