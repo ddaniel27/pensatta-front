52e6bbe9d677f3785e6187d43c0767230bc6d657
Daniel Dorado <danieldorado27@gmail.com>
2023-02-23 12:55:39 -0500

    [UPDATE] Code style for selectExercise

--- src/routes/components/selectExercise.jsx
+++ src/routes/components/selectExercise.jsx
@@ -1,32 +1,31 @@
-import React from "react"
-import data from "../../exercisesList.json"
-import "../../styles/selectExercise.css"
+import React from 'react'
+import data from '../../exercisesList.json'
+import '../../styles/selectExercise.css'
 
-export default function SelectExercise({setId, disableButton}){
-    
-    const [exercises] = React.useState(data.exercisesIds)
-    React.useEffect(()=>{
-        setId(0)
-        disableButton(true)
-    },[setId, disableButton])
+export default function SelectExercise ({ setId, disableButton }) {
+  const [exercises] = React.useState(data.exercisesIds)
+  React.useEffect(() => {
+    setId(0)
+    disableButton(true)
+  }, [setId, disableButton])
 
-    const handleChange = ({target}) => {
-        if(setId && disableButton){
-            setId(target.value === "test" ? "test" : Number(target.value))
-            disableButton(false)
-        }
+  const handleChange = ({ target }) => {
+    if (setId && disableButton) {
+      setId(target.value === 'test' ? 'test' : Number(target.value))
+      disableButton(false)
     }
+  }
 
-    return(
-        <div className="exercise-selector-area">
-            <select defaultValue={0} onChange={handleChange}>
-                <option value="0" hidden disabled>Select an exercise</option>
-                {
-                    exercises.map((exercise, index) => (
-                        <option key={index} value={exercise}> Ejercicio {exercise}</option>
-                    ))
-                }
-            </select>
-        </div>
-    )
+  return (
+    <div className='exercise-selector-area'>
+      <select defaultValue={0} onChange={handleChange}>
+        <option value='0' hidden disabled>Select an exercise</option>
+        {
+          exercises.map((exercise, index) => (
+            <option key={index} value={exercise}> Ejercicio {exercise}</option>
+          ))
+        }
+      </select>
+    </div>
+  )
 }
