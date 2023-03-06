7e53bbe185b2d714270085ec04bcc197e3c238ef
Daniel Dorado <danieldorado27@gmail.com>
2023-03-06 07:54:14 -0500

    [UPDATE] Code style fix

--- src/routes/components/studentProfile/studentProfileResumen.jsx
+++ src/routes/components/studentProfile/studentProfileResumen.jsx
@@ -1,17 +1,16 @@
 import { useState, useContext, useEffect } from 'react'
-import ProfileCard from "./profileCard"
+import ProfileCard from './profileCard'
 import ProgressBar from './progressBar'
-import BadgeDisplayer from "./badgeDisplayer"
+import BadgeDisplayer from './badgeDisplayer'
 import TableDisplayExercises from './tableDisplayExercises'
 import UserContext from '../../../context/UserContext'
 import { getResumen } from '../../../requests'
 import '../../../styles/studentProfileResume.css'
 
-export default function StudentProfileResume({ toggleView }) {
-  const [ resumenData, setResumenData ] = useState({})
-  const [ registers, setRegisters ] = useState([])
+export default function StudentProfileResume ({ toggleView }) {
+  const [resumenData, setResumenData] = useState({})
+  const [registers, setRegisters] = useState([])
   const { loginUser } = useContext(UserContext)
-
 
   const callHistoryEndpoint = async (user) => {
     getResumen(user, setResumenData)
@@ -22,9 +21,9 @@ export default function StudentProfileResume({ toggleView }) {
   }, [])
 
   useEffect(() => {
-    if(resumenData.resumen) {
+    if (resumenData.resumen) {
       const timeToSeconds = Math.round(resumenData.resumen.average_time / 1000)
-      const timeStr = `${Math.floor(timeToSeconds/60)}:${timeToSeconds%60 < 10 ? "0"+timeToSeconds%60 : timeToSeconds%60}`
+      const timeStr = `${Math.floor(timeToSeconds / 60)}:${timeToSeconds % 60 < 10 ? '0' + timeToSeconds % 60 : timeToSeconds % 60}`
       const objeMaped = {
         exercise_id: resumenData.resumen.total_exercises,
         time: timeStr,
@@ -34,29 +33,28 @@ export default function StudentProfileResume({ toggleView }) {
     }
   }, [resumenData])
 
-
-  return(
-    <div className="student-profile-viewer">
-      <div className="student-profile-viewer-header-left">
-      	<ProfileCard {...loginUser} institution_code={resumenData.institution_name} />
+  return (
+    <div className='student-profile-viewer'>
+      <div className='student-profile-viewer-header-left'>
+        <ProfileCard {...loginUser} institution_code={resumenData.institution_name} />
       </div>
-      <div className="student-profile-viewer-header-right">
-          <span>Última conexión: { new Date(loginUser.last_login).toLocaleString() }</span>
-          <span className="level-span">Nivel { Math.ceil(loginUser.total_exercises/4) }</span>
+      <div className='student-profile-viewer-header-right'>
+        <span>Última conexión: {new Date(loginUser.last_login).toLocaleString()}</span>
+        <span className='level-span'>Nivel {Math.ceil(loginUser.total_exercises / 4)}</span>
       </div>
-      <div className="student-profile-viewer-body-left">
+      <div className='student-profile-viewer-body-left'>
         <ProgressBar actual={registers[0]?.exercise_id || loginUser.total_exercises} />
-        <TableDisplayExercises 
-          registers={registers} 
-          headers_titles={['Ejercicios', 'Tiempo (mm:ss)', 'Promedio (%)']} 
-          resumen={true} 
+        <TableDisplayExercises
+          registers={registers}
+          headers_titles={['Ejercicios', 'Tiempo (mm:ss)', 'Promedio (%)']}
+          resumen
         />
       </div>
-      <div className="student-profile-viewer-body-right">
-      	<BadgeDisplayer actual={registers[0]?.exercise_id || loginUser.total_exercises} />
+      <div className='student-profile-viewer-body-right'>
+        <BadgeDisplayer actual={registers[0]?.exercise_id || loginUser.total_exercises} />
       </div>
 
-      <button onClick={()=>{toggleView(true)}}>ESTADISTICAS</button>
-    </div>	
+      <button onClick={() => { toggleView(true) }}>ESTADISTICAS</button>
+    </div>
   )
 }
