7099a0bd873ef373efdeb1516bb5493f0d4d521c
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-03-11 22:08:32 -0500

    update daniel

--- src/routes/components/studentProfileViewer.jsx
+++ src/routes/components/studentProfileViewer.jsx
@@ -4,24 +4,23 @@ import StudentProfileMetrics from './studentProfile/studentProfileMetrics'
 import ActivityContext from '../../context/ActivityContext'
 import '../../styles/studentProfileViewer.css'
 
-export default function StudentProfileViewer() {
+export default function StudentProfileViewer () {
   const { setTitle } = useContext(ActivityContext)
-  const [ toggleView, setToggleView ] = useState(false)
+  const [toggleView, setToggleView] = useState(false)
   useEffect(() => {
     setTitle('Perfil')
 
     return () => {
       setTitle('HUB')
     }
-    
   }, [])
 
-  return(
+  return (
     <>
       {
-        toggleView ?
-        <StudentProfileMetrics toggleView={setToggleView} /> :
-        <StudentProfileResumen toggleView={setToggleView} />
+        toggleView
+          ? <StudentProfileMetrics toggleView={setToggleView} />
+          : <StudentProfileResumen toggleView={setToggleView} />
       }
     </>
   )
