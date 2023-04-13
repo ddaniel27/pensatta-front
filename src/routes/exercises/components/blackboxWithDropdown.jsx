0c8c845d6710b80607f758da6d19b0635e8b2f5f
ddaniel27 <danieldorado27@gmail.com>
2023-04-13 02:31:15 -0500

    [UPDATE] ex94 first screen is done, next step to create second screen and logic to go there

--- src/routes/exercises/components/blackboxWithDropdown.jsx
+++ src/routes/exercises/components/blackboxWithDropdown.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import GenericSelector from '../../components/genericSelector'
 import blackboxSrc from '../../../../public/images/exercises/40/blackbox.svg'
 import '../../../styles/blackboxWithDropdown.css'
@@ -9,13 +10,22 @@ const opts = [
   { value: 3, label: 'Dividir' }
 ]
 
-export default function BlackboxWithDropdown ({ inputs = [0, 0], idBox = 0, options = opts }) {
+export default function BlackboxWithDropdown ({ inputs = [0, 0], idBox = 0, options = opts, setOperationResult }) {
+  const [selectedOption, setSelectedOption] = useState(null)
+
+  useEffect(() => {
+    if (selectedOption === null) return
+    const [a, b] = inputs
+    const result = selectedOption === '0' ? a + b : selectedOption === '1' ? a - b : selectedOption === '2' ? a * b : a / b
+    if (setOperationResult) setOperationResult(result)
+  }, [selectedOption])
+
   return (
     <div className='blackbox-with-dropdown'>
       <img src={blackboxSrc} alt='blackbox' />
       <div className='blackbox-with-dropdown__selector-area'>
         <h2>Caja {idBox}: {inputs[0]} y {inputs[1]}</h2>
-        <GenericSelector options={options} />
+        <GenericSelector options={options} setCurrentValue={setSelectedOption} />
       </div>
     </div>
   )
--- src/routes/exercises/ex94/ex94.jsx
+++ src/routes/exercises/ex94/ex94.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import NoScoringComponent from '../components/noScoringComponent'
-import BlackboxDropdown from '../components/blackboxWithDropdown'
 import data from './data.json'
 import '../../../styles/ex94.css'
+import BlackboxWithDropdown from '../components/blackboxWithDropdown'
 
 export default function Ex94 () {
-  const [myData] = React.useState({
-    ...data
+  const [myData] = useState({
+    ...data,
+    options: data.options.sort(() => 0.5 - Math.random()).slice(0, 3)
   })
+
+  const [results, setResults] = useState({})
+
+  useEffect(() => {
+    console.log(results)
+  }, [results])
 
   return (
     <NoScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name}>
       {
         (setPhase) => (
-          <div className='ex94-container'>
-            <BlackboxDropdown />
-            <BlackboxDropdown />
-            <BlackboxDropdown />
-          </div>
+          <FirstScreen data={myData} setResults={setResults} />
         )
       }
     </NoScoringComponent>
+  )
+}
+
+function FirstScreen ({ data, setResults }) {
+  return (
+    <div className='ex94-container'>
+      {data.options.map((option, index) => (
+        <BlackboxWithDropdown key={index} inputs={option.inputs} idBox={index + 1} setOperationResult={(currentResult) => setResults(prev => ({ ...prev, [index + 1]: currentResult }))} />
+      ))}
+    </div>
   )
 }
