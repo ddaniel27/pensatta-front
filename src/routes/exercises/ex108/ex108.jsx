e68800550eebc684d69da8b4905e7dbfeb5205e3
ddaniel27 <danieldorado27@gmail.com>
2023-04-13 01:31:39 -0500

    [FINISHED] Ex108 done, maybe some styles changes needed

--- src/routes/exercises/ex108/ex108.jsx
+++ src/routes/exercises/ex108/ex108.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import ScoringComponent from '../components/scoringComponent'
 import DndVonListComponent from '../components/dndFor108'
 import data from './data.json'
@@ -11,10 +11,29 @@ export default function Ex15 () {
   })
 
   const [values, setValues] = useState([])
+  const [isDone, setIsDone] = useState(false)
 
-  useEffect(() => {
-    console.log(values)
-  }, [values])
+  const checkResult = () => {
+    let score = 0
+    const listOfItems = document.querySelectorAll('.draggable-108-item')
+    values.forEach((value, index) => {
+      if (value !== myData.variations.sequence[index].id) {
+        listOfItems[index].classList.remove('correct-108')
+        listOfItems[index].classList.add('wrong-108')
+      } else {
+        listOfItems[index].classList.remove('wrong-108')
+        listOfItems[index].classList.add('correct-108')
+        score++
+      }
+    })
+    return score
+  }
+
+  const handleClick = (cb) => {
+    const score = checkResult()
+    cb(score)
+    setIsDone(true)
+  }
 
   return (
     <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name} threshold={myData.threshold} exerciseId={myData.id}>
@@ -22,6 +41,8 @@ export default function Ex15 () {
         (setScore, setPhase) => (
           <div className='list-ex108'>
             <DndVonListComponent data={myData.variations} returnScore={setValues} />
+            {!isDone && <button className='btn btn-primary' onClick={() => handleClick(setScore)}>RESPONDER</button>}
+            {isDone && <button className='btn btn-primary' onClick={() => setPhase('end')}>CONTINUAR</button>}
           </div>
         )
       }
