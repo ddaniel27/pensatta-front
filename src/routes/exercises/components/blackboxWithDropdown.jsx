1a9f96a3bf5d25c44d5a8366d9e5ae252bf0895a
ddaniel27 <danieldorado27@gmail.com>
2023-04-13 02:03:14 -0500

    [CREATE] ex94 created, also styles added for first screen

--- src/routes/exercises/components/blackboxWithDropdown.jsx
+++ src/routes/exercises/components/blackboxWithDropdown.jsx
@@ -1,10 +1,21 @@
+import GenericSelector from '../../components/genericSelector'
 import blackboxSrc from '../../../../public/images/exercises/40/blackbox.svg'
+import '../../../styles/blackboxWithDropdown.css'
 
-export default function BlackboxWithDropdown () {
+const opts = [
+  { value: 0, label: 'Sumar' },
+  { value: 1, label: 'Restar' },
+  { value: 2, label: 'Multiplicar' },
+  { value: 3, label: 'Dividir' }
+]
+
+export default function BlackboxWithDropdown ({ inputs = [0, 0], idBox = 0, options = opts }) {
   return (
     <div className='blackbox-with-dropdown'>
       <img src={blackboxSrc} alt='blackbox' />
-      <div className='blackbox-with-dropdown__dropdown'>
+      <div className='blackbox-with-dropdown__selector-area'>
+        <h2>Caja {idBox}: {inputs[0]} y {inputs[1]}</h2>
+        <GenericSelector options={options} />
       </div>
     </div>
   )
--- src/routes/exercises/ex94/ex94.jsx
+++ src/routes/exercises/ex94/ex94.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
-import ActivityContext from '../../../context/ActivityContext'
 import NoScoringComponent from '../components/noScoringComponent'
 import BlackboxDropdown from '../components/blackboxWithDropdown'
 import data from './data.json'
+import '../../../styles/ex94.css'
 
 export default function Ex94 () {
-  const { setActivity, setTitle, setBackground } = React.useContext(ActivityContext)
   const [myData] = React.useState({
     ...data
   })
-
-  React.useEffect(() => {
-    setTitle(data.name)
-    setBackground(data.color)
-    return () => {
-      setActivity(true)
-    }
-  }, [setActivity, setBackground, setTitle])
 
   return (
     <NoScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name}>
       {
         (setPhase) => (
-          <>
-            <BlackboxDropdown setPhase={setPhase} />
-          </>
+          <div className='ex94-container'>
+            <BlackboxDropdown />
+            <BlackboxDropdown />
+            <BlackboxDropdown />
+          </div>
         )
       }
     </NoScoringComponent>
