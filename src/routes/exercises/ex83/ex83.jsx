f7963cfc4a92e2832c3180afb9c099e20ad741c0
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-04-30 21:25:21 -0500

    [update] ex83

--- src/routes/exercises/ex83/ex83.jsx
+++ src/routes/exercises/ex83/ex83.jsx
@@ -1,7 +1,16 @@
+import ScoringComponent from '../components/ScoringComponent'
+import DfdSelect from '../components/dfdSelect'
+import data from './data.json'
+
 export default function ex83 () {
+  const variations = data.variations[0]
   return (
-    <div>
-      <h1>ex83</h1>
-    </div>
+    <ScoringComponent initMessages={data.initMessages} title={data.name} background={data.color} exerciseId={data.id} threshold={data.threshold} >
+      {
+        (setScore, setPhase) => (
+          <DfdSelect purple={variations.purple} yellow={variations.yellow} gray1={variations.gray1} gray2={variations.gray2} gray3={variations.gray3} gray4={variations.gray4} corrects={variations.corrects} setScore={setScore} setPhase={setPhase} />
+        )
+      }
+    </ScoringComponent>
   )
 }
