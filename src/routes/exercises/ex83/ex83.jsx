d06a094a0ecfca46a732311921e93980d61f1367
ddaniel27 <danieldorado27@gmail.com>
2023-05-18 16:35:55 -0500

    [UPDATE] Added ex83 image

--- src/routes/exercises/ex83/ex83.jsx
+++ src/routes/exercises/ex83/ex83.jsx
@@ -1,6 +1,7 @@
 import ScoringComponent from '../components/ScoringComponent'
 import DfdSelect from '../components/dfdSelect'
 import data from './data.json'
+import '../../../styles/ex83.css'
 
 export default function ex83 () {
   const variations = data.variations[0]
@@ -8,7 +9,10 @@ export default function ex83 () {
     <ScoringComponent initMessages={data.initMessages} title={data.name} background={data.color} exerciseId={data.id} threshold={data.threshold}>
       {
         (setScore, setPhase) => (
-          <DfdSelect purple={variations.purple} yellow={variations.yellow} gray1={variations.gray1} gray2={variations.gray2} gray3={variations.gray3} gray4={variations.gray4} corrects={variations.corrects} setScore={setScore} setPhase={setPhase} />
+          <div className='ex83-container'>
+            <img src={`/images/exercises/83/${variations.image}`} />
+            <DfdSelect purple={variations.purple} yellow={variations.yellow} gray1={variations.gray1} gray2={variations.gray2} gray3={variations.gray3} gray4={variations.gray4} corrects={variations.corrects} setScore={setScore} setPhase={setPhase} />
+          </div>
         )
       }
     </ScoringComponent>
