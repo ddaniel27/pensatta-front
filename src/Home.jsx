cc550e21e56cdb2a24319142c031ac77009e4180
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-05-18 12:31:27 -0500

    merge back button

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+export default function Home () {
+  return (
+    <div style={{
+      position: 'relative',
+      width: '100%',
+      height: '0',
+      paddingTop: '259.1508%',
+      paddingBottom: '0',
+      boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+      marginTop: '1.6em',
+      marginBottom: '0.9em',
+      overflow: 'hidden',
+      overFlowY: 'scroll',
+      borderRadius: '8px',
+      willChange: 'transform'
+    }}>
+      <iframe style={{ overflowY: 'scroll', position: 'absolute', width: '100%', height: '100%', top: '0', left: '0', border: 'none', padding: '0', margin: '0' }}
+        src={'https://www.canva.com/design/DAFjFXbHh9E/view?embed'} allowfullscreen="allowfullscreen" allow="fullscreen" />
+
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Pdf from './routes/components/studentProfile/pdf'
+import Home from './Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
-   <BrowserRouter>
-      <Routes>
-         <Route path="/pensatta" element={<App />}/>
-         <Route path='/resumen/:id' element={<Pdf />} />
-         <Route path="*" element={<div>Not found</div>} />
-      </Routes>
-   </BrowserRouter>,
-   document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/pensatta" element={<App />}/>
+      <Route path='/resumen/:id' element={<Pdf />} />
+      <Route path='/' element={<Home />} />
+      <Route path="*" element={<div>Not found</div>} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 )
