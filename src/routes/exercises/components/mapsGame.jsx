fb1a331808ad7f2551cdf03cc499d74528cd758b
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-05-19 16:20:51 -0500

    [added] game 93 added

---
+++ src/routes/exercises/components/mapsGame.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+import NorthAmerica from './continentNorthAmerica'
+import CentralAmerica from './continentCentralAmerica'
+import SouthAmerica from './continentSouthAmerica'
+import NorthEurope from './continentNorthEurope'
+import CentralEurope from './continentCentralEurope'
+
+const MapsGame = () => {
+  return (
+    <>
+      <CentralEurope country={'irlanda'} />
+    </>
+
+  )
+}
+export default MapsGame
