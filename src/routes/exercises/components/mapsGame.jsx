188aa791c4c5011ec8d8ae175aed866db4b5613f
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-05-21 22:16:22 -0500

    [add] 50% 93 game

--- src/routes/exercises/components/mapsGame.jsx
+++ src/routes/exercises/components/mapsGame.jsx
@@ -10,10 +10,47 @@ import SeptentrionalAfrica from './continentSeptentrionalAfrica'
 import WestAfrica from './continentWestAfrica'
 import EastAfrica from './continentEastAfrica'
 import MeridionalAfrica from './continentMeridionalAfrica'
+import WestAsia from './continentWestAsia'
+import CentralAsia from './continentCentralAsia'
+import EastAsia from './continentEastAsia'
+import SouthAsia from './continentSouthAsia'
+import Oceania from './continentOceania'
+import AllWorld from './continentsAll'
+
+const SecondStage = ({ options = [] }) => {
+  const [country, setCountry] = useState('')
+  const [continent, setContinent] = useState('')
+
+  return (
+    <div>
+      <div>
+        {
+
+        }
+      </div>
+      <div>
+        <div>
+          <AllWorld setContinent={setContinent} continent={continent}/>
+        </div>
+        <div>
+          {
+            options.map((option, index) => (
+              <div key={index}>
+                <div>{option}</div>
+                <div>{}</div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const MapsGame = () => {
   return (
     <>
-      <MeridionalAfrica country={'namibia'} />
+      <SecondStage />
     </>
 
   )
