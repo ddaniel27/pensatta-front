38bcbef87f8d6610817f5407c309b54d5bca192b
ddaniel27 <danieldorado27@gmail.com>
2023-05-24 09:52:44 -0500

    [FIX] Ex51 bug solved

--- src/routes/exercises/components/dndListComponent.jsx
+++ src/routes/exercises/components/dndListComponent.jsx
@@ -1,73 +1,70 @@
-import React from "react"
-import { DragDropContext } from "react-beautiful-dnd"
-import DroppableListComponent from "./droppableListComponent"
-import "../../../styles/dndListComponent.css"
+import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
+import DroppableListComponent from './droppableListComponent'
+import '../../../styles/dndListComponent.css'
 
+export default function DndListComponent ({ data, returnScore, reset, showCorrect = false }) {
+  const [myData, setmyData] = React.useState(data)
 
-export default function DndListComponent({data, returnScore, reset, showCorrect = false}) {
-    const [myData, setmyData] = React.useState(data)
+  React.useEffect(() => {
+    if (returnScore) {
+      const returnData = Object.values(myData.columns).map(column => column.optionsIds[0])
+      returnScore(returnData)
+    }
+  }, [myData])
 
-    React.useEffect(() => {
-        if(returnScore){
-            let returnData = Object.values(myData.columns).map(column => column.optionsIds[0])
-            returnScore(returnData)
-        }
+  React.useEffect(() => {
+    if (reset) {
+      setmyData(data)
+    }
+  }, [reset])
 
-    }, [myData])
+  const onDragEnd = result => {
+    const { destination, source, draggableId } = result
+    if (!destination) { return }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) { return }
 
-    React.useEffect(() => {
-        if(reset){
-            setmyData(data)
-        }
-    }, [reset])
+    const start = myData.columns[source.droppableId]
+    const finish = myData.columns[destination.droppableId]
 
-    const onDragEnd = result => {
-        const {destination, source, draggableId } = result
-        if(!destination) { return }
-        if (destination.droppableId === source.droppableId && destination.index === source.index) { return }
+    const startOptionsIds = Array.from(start.optionsIds)
+    const aux = startOptionsIds.splice(source.index, 1)
 
-        const start = myData.columns[source.droppableId]
-        const finish = myData.columns[destination.droppableId]
+    const finishOptionsIds = Array.from(finish.optionsIds)
+    const aux2 = finishOptionsIds.splice(destination.index, 1)
 
-       
-        const startOptionsIds = Array.from(start.optionsIds)
-        let aux = startOptionsIds.splice(source.index, 1)
+    startOptionsIds.splice(destination.index, 0, aux2[0])
+    finishOptionsIds.splice(source.index, 0, aux[0])
 
-        const finishOptionsIds = Array.from(finish.optionsIds)
-        let aux2 = finishOptionsIds.splice(destination.index, 1)
-
-        startOptionsIds.splice(destination.index, 0, aux2[0])
-        finishOptionsIds.splice(source.index, 0, aux[0])
-
-        const newStart = {
-            ...start,
-            optionsIds: startOptionsIds
-        }
-
-        const newFinish = {
-            ...finish,
-            optionsIds: finishOptionsIds
-        }
-
-        setmyData(prevState=>(
-            {...prevState,
-                columns:{
-                ...prevState.columns,
-                [newStart.id]: newStart,
-                [newFinish.id]: newFinish
-                }
-            }
-        ))
-        
+    const newStart = {
+      ...start,
+      optionsIds: startOptionsIds
     }
 
-    return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            <div className="drag-drop-list-context">
-                <DroppableListComponent showCorrect={showCorrect} column={myData.columns["column-1"]} options={myData.columns["column-1"].optionsIds.map(optionId => myData.options[optionId])} />
-                <DroppableListComponent showCorrect={showCorrect} column={myData.columns["column-2"]} options={myData.columns["column-2"].optionsIds.map(optionId => myData.options[optionId])} />
-                <DroppableListComponent showCorrect={showCorrect} column={myData.columns["column-3"]} options={myData.columns["column-3"].optionsIds.map(optionId => myData.options[optionId])} />
-            </div>
-        </DragDropContext>
-    )
+    const newFinish = {
+      ...finish,
+      optionsIds: finishOptionsIds
+    }
+
+    setmyData(prevState => (
+      {
+        ...prevState,
+        columns: {
+          ...prevState.columns,
+          [newStart.id]: newStart,
+          [newFinish.id]: newFinish
+        }
+      }
+    ))
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className='drag-drop-list-context'>
+        <DroppableListComponent showCorrect={showCorrect} column={myData.columns['column-1']} options={myData.columns['column-1'].optionsIds.map(optionId => myData.options[optionId])} />
+        <DroppableListComponent showCorrect={showCorrect} column={myData.columns['column-2']} options={myData.columns['column-2'].optionsIds.map(optionId => myData.options[optionId])} />
+        <DroppableListComponent showCorrect={showCorrect} column={myData.columns['column-3']} options={myData.columns['column-3'].optionsIds.map(optionId => myData.options[optionId])} />
+      </div>
+    </DragDropContext>
+  )
 }
--- src/routes/exercises/ex51/ex51.jsx
+++ src/routes/exercises/ex51/ex51.jsx
@@ -1,66 +1,59 @@
-import React from "react"
-import ScoringComponent from "../components/scoringComponent"
-import DndListComponent from "../components/dndListComponent"
-import data from "./data.json"
-import "../../../styles/ex17.css"
+import { useLayoutEffect, useState } from 'react'
+import ScoringComponent from '../components/scoringComponent'
+import DndListComponent from '../components/dndListComponent'
+import data from './data.json'
+import '../../../styles/ex17.css'
 
-export default function Ex51(){
+export default function Ex51 () {
+  const [myData] = useState({
+    ...data,
+    options: data.variations.sort(() => Math.random() - 0.5).slice(0, 1)[0]
+  })
+  const [reset, setReset] = useState(false)
 
-    // eslint-disable-next-line no-unused-vars
-    const [ myData, setMyData ] = React.useState({
-        ...data,
-        options: data.variations.sort(() => Math.random() - 0.5).slice(0,1)[0]
-    })
-    const [start, setStart] = React.useState(false)
-    const [reset, setReset] = React.useState(false)
+  const [optionsData, setOptionsData] = useState({})
 
-    const [ optionsData, setOptionsData ] = React.useState({})
-
-    React.useLayoutEffect(() => {
-        if(reset){
-            setReset(false)
-        }
-    }, [optionsData])
-
-
-    const handleClick = (cb1,cb2)=>{
-        document.querySelector(`.maze-list-style`).scrollTop = 0;
-        setStart(true)
-        let flag = true
-        for(let i = 0; i < myData.sequence.length; i++){
-            if(optionsData[i] !== myData.sequence[i]){
-                flag = false
-            }
-        }
-        if(flag){
-            cb1(1)
-        }else{
-            cb1(0)
-        }
-        cb2("end")
+  useLayoutEffect(() => {
+    if (reset) {
+      setReset(false)
     }
+  }, [optionsData])
 
-    const handleReset = () => {
-        setStart(false)
-        setOptionsData({})
-        setReset(true)
+  const handleClick = (cb1, cb2) => {
+    document.querySelector('.maze-list-style').scrollTop = 0
+    let flag = true
+    for (let i = 0; i < myData.sequence.length; i++) {
+      if (optionsData[i] !== myData.sequence[i]) {
+        flag = false
+      }
     }
+    if (flag) {
+      cb1(1)
+    } else {
+      cb1(0)
+    }
+    cb2('end')
+  }
 
+  const handleReset = () => {
+    setOptionsData({})
+    setReset(true)
+  }
 
-    return(
-        <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name} threshold={myData.threshold} exerciseId={myData.id}>
-            {
-                (setScore,setPhase) => (
-                    <div className="maze-list-style">
-                        <DndListComponent data={myData} returnScore={setOptionsData} reset={reset} />
-                       
-                        <div className="buttons-field">
-                            {optionsData.length > 0 && <button onClick={handleReset} className="restart">REINTENTAR</button>}
-                            <button onClick={()=>{handleClick(setScore,setPhase)}}>INICIAR</button>
-                        </div>
+  return (
+    <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name} threshold={myData.threshold} exerciseId={myData.id}>
+      {
+                (setScore, setPhase) => (
+                  <div className='maze-list-style'>
+                    <DndListComponent data={myData.options} returnScore={setOptionsData} reset={reset} />
+
+                    <div className='buttons-field'>
+                      {optionsData.length > 0 && <button onClick={handleReset} className='restart'>REINTENTAR</button>}
+                      <button onClick={() => { handleClick(setScore, setPhase) }}>RESPONDER</button>
                     </div>
+                  </div>
                 )
             }
-        </ScoringComponent>
-    )
+    </ScoringComponent>
+  )
 }
