1b2f1b15a87f5207b6ea10c89ec401b41bc38d4c
ddaniel27 <danieldorado27@gmail.com>
2023-06-27 12:24:54 -0500

    [UPDATE] Dropdowns added to ex74

--- src/routes/exercises/components/selectorWithArrow.jsx
+++ src/routes/exercises/components/selectorWithArrow.jsx
@@ -1,30 +1,29 @@
-import React from "react"
-import "../../../styles/selectorWithArrow.css"
+import React from 'react'
+import '../../../styles/selectorWithArrow.css'
 
-export default function SelectorWithArrow({title="Field", options=[], min=0, max=3, initSelected=0, returnValue}){
+export default function SelectorWithArrow ({ title = 'Field', options = [], min = 0, max = 3, initSelected = 0, returnValue }) {
+  const [selected, setSelected] = React.useState(initSelected)
 
-    const [selected, setSelected] = React.useState(initSelected)
-
-    React.useEffect(()=>{
-        if(returnValue){
-            returnValue({value:options[selected], index:selected})
-        }
-    }, [selected])
-
-    const handleIncrement = () => {
-        setSelected(selected < max ? selected + 1 : min)
+  React.useEffect(() => {
+    if (returnValue) {
+      returnValue({ value: options[selected], index: selected })
     }
+  }, [selected])
 
-    const handleDecrement = () => {
-        setSelected(selected > min ? selected - 1 : max)
-    }
+  const handleIncrement = () => {
+    setSelected(selected < max ? selected + 1 : min)
+  }
 
-    return(
-        <div className="selector-field-item">
-            <h2>{title}</h2>
-            <span className="arrow-select reverse" value={title} onClick={handleDecrement}></span>
-            <span className="current-value">{options[selected]}</span>
-            <span className="arrow-select" value={title} onClick={handleIncrement}></span>            
-        </div>
-    )
+  const handleDecrement = () => {
+    setSelected(selected > min ? selected - 1 : max)
+  }
+
+  return (
+    <div className='selector-field-item'>
+      <h2>{title}</h2>
+      <span className='arrow-select reverse' value={title} onClick={handleDecrement} />
+      <span className='current-value'>{options[selected]}</span>
+      <span className='arrow-select' value={title} onClick={handleIncrement} />
+    </div>
+  )
 }
