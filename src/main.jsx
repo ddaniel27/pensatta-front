81bfa6cb63b4b710c5aa6aacd524911561fe2e36
ddaniel27 <danieldorado27@gmail.com>
2023-09-26 18:21:44 -0500

    [CREATE] Added lang code module

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Pdf from './routes/components/studentProfile/pdf'
+import './lang-config'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/pensatta" element={<App />}/>
-      <Route path='/resumen/:id' element={<Pdf />} />
-      <Route path="*" element={<div>Not found</div>} />
-    </Routes>
-  </BrowserRouter>,
+  <React.Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pensatta" element={<App />}/>
+        <Route path='/resumen/:id' element={<Pdf />} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.Suspense>,
   document.getElementById('root')
 )
