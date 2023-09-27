bbae8661b3792f59f267cd5d504272147c63262f
ddaniel27 <danieldorado27@gmail.com>
2023-09-26 20:40:22 -0500

    [UPDATE] Lang update with ex01

--- src/routes/components/header.jsx
+++ src/routes/components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuHam from './menuHam'
+import { useTranslation } from 'react-i18next'
 import ActivityContext from '../../context/ActivityContext'
 import '../../styles/header.css'
 
@@ -7,6 +8,7 @@ export default function Header({ headerText }){
 
     const { setActivity, setProfile } = React.useContext(ActivityContext)
     const [menuHam, setMenuHam] = React.useState(false)
+    const { i18n } = useTranslation()
 
     const toggleMenuHam = () => {
         setMenuHam(true)
@@ -17,11 +19,18 @@ export default function Header({ headerText }){
         setProfile(false)
     }
 
+    const handleChangeLanguage = () => {
+      i18n.changeLanguage(i18n.language === 'es' ? 'en' : 'es')
+    }
+
     return(
         <header>
             <img src='./images/Atomo_Pensatta-Símbolo.svg' alt='Atomo Pensatta' onClick={handleReset}/>
             <h1>{headerText}</h1>
-            <img src='./images/Atomo_Icono_MenúHamburguesa.svg' alt='Menú' onClick={toggleMenuHam}/>
+            <div>
+              <img src='./images/Atomo_Icono_MenúHamburguesa.svg' alt='Menú' onClick={toggleMenuHam}/>
+              <button className="header__language__selector" onClick={handleChangeLanguage}>{i18n.language}</button>
+            </div>
             {menuHam && <MenuHam setShow={setMenuHam} />}
         </header>
     )
