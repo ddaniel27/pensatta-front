104c8b328253e45642b26bdf8fe531d975f053cd
ddaniel27 <danieldorado27@gmail.com>
2023-09-29 08:37:38 -0500

    [UPDATE] Lang selector just for admin view

--- src/routes/components/header.jsx
+++ src/routes/components/header.jsx
@@ -2,11 +2,13 @@ import { useState, useContext } from 'react'
 import MenuHam from './menuHam'
 import { useTranslation } from 'react-i18next'
 import ActivityContext from '../../context/ActivityContext'
+import UserContext from '../../context/UserContext'
 import '../../styles/header.css'
 
 export default function Header({ headerText }){
 
     const { setActivity, setProfile } = useContext(ActivityContext)
+  const { loginUser } = useContext(UserContext)
     const [menuHam, setMenuHam] = useState(false)
     const { i18n } = useTranslation()
 
@@ -29,11 +31,14 @@ export default function Header({ headerText }){
             <h1>{headerText}</h1>
             <div className="ham-div">
               <img src='./images/Atomo_Icono_MenúHamburguesa.svg' alt='Menú' onClick={toggleMenuHam}/>
-              <select className="select-header-lang" name="type" onChange={handleChangeLanguage} >
-                <option value="es">ES</option>
-                <option value="en">EN</option>
-                <option value="pt">PT</option>
-              </select>
+              {
+                loginUser.role.toLowerCase() === 'admin' &&
+                <select className="select-header-lang" name="type" onChange={handleChangeLanguage} >
+                  <option value="es">ES</option>
+                  <option value="en">EN</option>
+                  <option value="pt">PT</option>
+                </select>
+              }
             </div>
             {menuHam && <MenuHam setShow={setMenuHam} />}
         </header>
