674e14d8e93d2f2b9aa97e68de212ce6594ee0dc
jmcc153 <jmcastillo9915@gmail.com>
2024-03-05 11:58:34 -0500

    [Fix] translates ex109 to 116

--- src/routes/exercises/ex115/ex115.jsx
+++ src/routes/exercises/ex115/ex115.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ScreenRenderUniqueOptionsEncapsulate from '../components/screenRenderUniqueOptionsEncapsulate'
 import ScoringComponent from '../components/scoringComponent'
 import useData from '../../../hooks/useData'
 
 export default function Ex115 () {
   const { data } = useData('ex115')
-  const [myData] = React.useState({
+  const [myData, setMyData] = React.useState({
     ...data,
     options: data.options.sort(() => 0.5 - Math.random()).slice(0, data.threshold.perfect)
   })
+
+  useEffect(() => {
+    setMyData({
+      ...data,
+      options: data.options.sort(() => 0.5 - Math.random()).slice(0, data.threshold.perfect)
+    })
+  }, [data])
 
   return (
     <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.name} threshold={myData.threshold} exerciseId={myData.id}>
