499170fd6788bcbcdc22d632a6a10f5a54a98475
jmcc153 <jmcastillo9915@gmail.com>
2024-03-20 13:57:21 -0500

    [Add] images ex110, [Modify] Logic ex110 and ex48

--- src/routes/exercises/ex110/ex110.jsx
+++ src/routes/exercises/ex110/ex110.jsx
@@ -1,16 +1,15 @@
-import {useEffect, useState} from "react"
-import MazeCardsComponent from "../components/mazeCardsComponent"
-import ScoringComponent from "../components/scoringComponent"
-import useData from "../../../hooks/useData"
+import { useEffect, useState } from 'react'
+import MazeCardsComponent from '../components/mazeCardsComponent'
+import ScoringComponent from '../components/scoringComponent'
+import useData from '../../../hooks/useData'
 
 const Ex110 = () => {
-    const [lab, setLab] = useState(Math.floor(Math.random() * 6) + 1);
-    useEffect(()=>{
-        setLab(Math.floor(Math.random() * 6) + 1)
-        
-    },[])
+  const [lab, setLab] = useState(Math.floor(Math.random() * 6) + 1)
+  useEffect(() => {
+    setLab(Math.floor(Math.random() * 6) + 1)
+  }, [])
 
-  const { data } = useData("ex110")
+  const { data } = useData('ex110')
   const [myData, setMyData] = useState({
     ...data,
     options: data.options.sort(() => 0.5 - Math.random()).slice(0, 1)[0]
@@ -24,17 +23,16 @@ const Ex110 = () => {
     })
   }, [data])
 
-    return(
-        <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.options.title} threshold={myData.threshold} exerciseId={myData.id}>
-            {
-                (setScore, setPhase) => (
-                    <MazeCardsComponent cardsArray={myData.options.cards} setPhase={setPhase} setScore={setScore}/>
-                )
-            }
-        </ScoringComponent>
-        
-    )
+  return (
+    <ScoringComponent initMessages={myData.initMessages} background={myData.color} title={myData.options.title} threshold={myData.threshold} exerciseId={myData.id}>
+      {
+        (setScore, setPhase) => (
+          <MazeCardsComponent cardsArray={myData.options.cards} setPhase={setPhase} setScore={setScore}/>
+        )
+      }
+    </ScoringComponent>
 
+  )
 }
 
 export default Ex110
